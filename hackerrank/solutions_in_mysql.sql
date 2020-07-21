47968333c4571071e1b5e5f6fcf47b366e4e5c89
Dewan Mahmud Raihan <Raihan92@users.noreply.github.com>
2020-07-21 15:48:30 +0600

    Update and rename solved_sql.sql to solutions_in_mysql.sql

---
+++ hackerrank/solutions_in_mysql.sql
@@ -0,0 +1,158 @@
+1. https://www.hackerrank.com/challenges/weather-observation-station-5/problem`
+Solution:
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY ASC LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1;
+
+2. https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+Solution:
+SELECT DISTINCT(CITY) FROM STATION 
+WHERE UPPER(SUBSTR(CITY, 1, 1)) IN ('A','E','I','O','U')
+AND UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) IN ('A','E','I','O','U');
+
+3. https://www.hackerrank.com/challenges/more-than-75-marks/problem
+Solution:
+SELECT NAME FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY SUBSTR(NAME, LENGTH(NAME)-2, 3), ID ASC;
+
+4. https://www.hackerrank.com/challenges/the-pads/problem
+Solution:
+SELECT CONCAT( NAME, CONCAT(CONCAT('(', SUBSTR(OCCUPATION, 1, 1)), ')')) 
+FROM OCCUPATIONS
+ORDER BY NAME ASC;
+
+SELECT CONCAT('THERE ARE A TOTAL OF ', COUNT(OCCUPATION)), CONCAT(LOWER(OCCUPATION),'S.') 
+FROM OCCUPATIONS 
+GROUP BY OCCUPATION
+ORDER BY COUNT(OCCUPATION), OCCUPATION ASC;
+
+5. https://www.hackerrank.com/challenges/occupations/problem
+Solution:
+SET @r1=0, @r2=0, @r3 =0, @r4=0;
+SELECT min(CASE WHEN occupation = 'doctor' THEN name END),
+       min(CASE WHEN occupation = 'professor' THEN name END),
+       min(CASE WHEN occupation = 'singer' THEN name END),
+       min(CASE WHEN occupation = 'actor' THEN name END)
+  FROM (SELECT name,
+               occupation,
+               CASE occupation
+                  WHEN 'Doctor' THEN @r1 := @r1 + 1
+                  WHEN 'Professor' THEN @r2 := @r2 + 1
+                  WHEN 'Singer' THEN @r3 := @r3 + 1
+                  WHEN 'Actor' THEN @r4 := @r4 + 1
+               END
+                  AS rn
+          FROM OCCUPATIONS
+        ORDER BY Name) AS t
+GROUP BY rn;
+
+6. https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+Solution:
+SELECT BT.N,
+    CASE
+        WHEN BT.P IS NULL THEN 'Root'
+        WHEN EXISTS (SELECT B.P FROM BST B WHERE B.P = BT.N) THEN 'Inner'        
+        ELSE 'Leaf'
+    END
+FROM BST AS BT 
+ORDER BY BT.N;
+
+7. https://www.hackerrank.com/challenges/the-company/problem
+Solution:
+SELECT cc.company_code,
+    cc.founder,
+    COUNT(distinct lm.lead_manager_code),
+    COUNT(distinct sm.senior_manager_code),
+    COUNT(distinct mg.manager_code),
+    COUNT(distinct em.employee_code)
+FROM
+    company cc
+LEFT JOIN
+    lead_manager lm
+    ON (lm.company_code = cc.company_code)
+LEFT JOIN 
+    senior_manager sm
+    ON (sm.lead_manager_code = lm.lead_manager_code)
+LEFT JOIN 
+    manager mg
+    ON (mg.senior_manager_code = sm.senior_manager_code)
+LEFT JOIN 
+    employee em
+    ON (em.manager_code = mg.manager_code)
+GROUP BY
+    cc.company_code, cc.founder
+ORDER BY
+    cc.company_code;
+
+8. https://www.hackerrank.com/challenges/weather-observation-station-18/problem
+Solution:
+SELECT ROUND(MIN_DIFF_LAT_N + MIN_DIFF_LONG_W,4)
+FROM (
+    SELECT
+        abs(min(LAT_N) - max(LAT_N)) as MIN_DIFF_LAT_N,
+        abs(min(LONG_W) - max(LONG_W)) as MIN_DIFF_LONG_W
+    from station
+) ST;
+
+9. https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+Solution:
+SELECT ROUND(SQRT(MIN_DIFF_LAT_N*MIN_DIFF_LAT_N + MIN_DIFF_LONG_W*MIN_DIFF_LONG_W),4)
+FROM (
+    SELECT
+        abs(min(LAT_N) - max(LAT_N)) as MIN_DIFF_LAT_N,
+        abs(min(LONG_W) - max(LONG_W)) as MIN_DIFF_LONG_W
+    from station
+) ST;
+
+10. https://www.hackerrank.com/challenges/weather-observation-station-20/problem
+Solution:
+SET @r=0, @total_rows=0;
+SELECT ROUND(ST.LAT_N,4)
+FROM (
+    SELECT
+        LAT_N, 
+        @r:=@r+1 as row_number, 
+        @total_rows := @r
+    from station
+    ORDER BY LAT_N ASC
+) ST
+WHERE ST.row_number = (@total_rows+1)/2;
+
+11. https://www.hackerrank.com/challenges/the-report/problem
+Solution:
+SELECT
+    CASE
+        WHEN ST.MARKS <= 70 THEN NULL
+        ELSE ST.NAME
+    END, 
+    (
+        SELECT 
+            GRADE
+        FROM GRADES GD
+        WHERE ST.MARKS >= GD.MIN_MARK AND ST.MARKS <= GD.MAX_MARK
+    ) AS C_GD,
+    ST.MARKS
+FROM STUDENTS ST
+ORDER BY C_GD DESC, ST.NAME ASC;
+
+12. https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+Solution:
+SELECT
+    WD.id,
+    WD_P.age,
+    WD.coins_needed,
+    WD.power
+FROM wands AS WD
+LEFT JOIN wands_property AS WD_P
+ON WD.code = WD_P.code
+WHERE WD_P.is_evil = 0
+AND WD.coins_needed = 
+    (
+        SELECT min(coins_needed) 
+        FROM wands as WD1
+        LEFT JOIN wands_property as WD_P1 
+        ON WD1.code = WD_P1.code 
+        WHERE WD1.power = WD.power 
+        AND WD_P1.age = WD_P.age
+    )
+ORDER BY WD.power DESC, WD_P.age DESC;
--- hackerrank/solved_sql.sql
+++
@@ -1,7 +0,0 @@
-1. Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem
-Solution:
-SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
-
-2. Problem: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
-Solution:
-SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
